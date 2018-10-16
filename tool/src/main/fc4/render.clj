22aa2a5c501e30511887c9c37f4c8c0d2751859b
Avi Flax <avi.flax@fundingcircle.com>
2018-10-15 22:11:16 -0400

    WIP++ JGraph rendering… text is awful-looking tho

--- tool/src/main/fc4/render.clj
+++ tool/src/main/fc4/render.clj
@@ -19,9 +19,9 @@
 (def vertex-height 60)
 
 (defn vertex-label-text [{:keys [name description] :as element}]
-  (str "<b>" name "</b>"
+  (str "<p style='width: 50px;'><b>" name "</b>"
        (when description
-             (str "\n<span style=\"font-size: smaller;\">" description "</span>"))))
+             (str "<br><span style='font-size: 60%;'>" description "</span></p>"))))
 
 (defn add-vertices
   ;; TODO: add shape=actor; if the element is of type Person, and change the dimensions
@@ -34,7 +34,7 @@
             root (.getDefaultParent graph)
             [x y] (->> (parse-coords (:position e))
                        (map (partial * scale)))
-            style "whiteSpace=wrap;fontSize=8;labelBackgroundColor=#000000;"]
+            style "fontSize=8;whiteSpace=wrap;labelBackgroundColor=none;"]
         [name (.insertVertex graph root id value x y vertex-width vertex-height style)]))))
 
 (defn edge-label-text [{:keys [description technology] :as relationship}]
@@ -59,11 +59,11 @@
            java.util.ArrayList.
            (.setPoints geo)))))
 
-(defn doc->graph [d]
+(defn diagram->graph [diagram]
   (let [graph (new-graph)
         root (.getDefaultParent graph)
-        vertices (add-vertices d graph)]
-    (add-edges d graph vertices)
+        vertices (add-vertices diagram graph)]
+    (add-edges diagram graph vertices)
     graph))
 
 (defn render-image [graph]
@@ -81,14 +81,17 @@
   (System/setProperty "apple.awt.UIElement" "true")
   (ImageIO/write image "PNG" (io/file "/tmp/graph.png")))
 
-(defn render-swing [d]
+(defn render-swing [{:keys [:type :scope] :as diagram}]
   (System/setProperty "apple.awt.UIElement" "false")
-  (let [graph (doc->graph d)
-        {:keys [:type :scope]} d
+  (let [graph (diagram->graph diagram)
         title (str type " for " scope)
-        frame (sc/frame :title title)]
-    (->> (mxGraphComponent. graph)
-         sc/scrollable
+        frame (sc/frame :title title)
+        gc (doto (mxGraphComponent. graph)
+                 (.setPageScale 5)
+                 (.setZoomFactor 2)
+                 (.zoomIn))]
+    (.setScale (.getCanvas gc) scale)
+    (->> (sc/scrollable gc)
          (sc/config! frame :content))
     (sc/invoke-later
       (-> frame sc/pack! sc/show!))))
@@ -97,5 +100,5 @@
   (use 'fc4.render)
   (def fc (slurp "examples/internet_banking_context.yaml")) ; fc = file-content
   (-> fc se/process-file ::se/main-processed render-swing)
-  (-> fc se/process-file ::se/main-processed doc->graph render-image save-png)
+  (-> fc se/process-file ::se/main-processed diagram->graph render-image save-png)
   )
