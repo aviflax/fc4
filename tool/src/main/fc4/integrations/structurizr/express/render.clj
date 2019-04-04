995a846cc7f0d0acc69b3809ea2ffa49c8cd4395
Avi Flax <avi.flax@fundingcircle.com>
2019-04-04 16:32:37 -0400

    WIP Remove Node!

--- tool/src/main/fc4/integrations/structurizr/express/render.clj
+++ tool/src/main/fc4/integrations/structurizr/express/render.clj
@@ -1,5 +1,9 @@
 (ns fc4.integrations.structurizr.express.render
-  (:require [clojure.java.io      :as io      :refer [file]]
+  (:require [clj-chrome-devtools.commands.dom :as dom]
+            [clj-chrome-devtools.commands.page :as page]
+            [clj-chrome-devtools.core :as chrome]
+            [clj-chrome-devtools.commands.runtime :as js]
+            [clojure.java.io      :as io      :refer [file]]
             [clojure.java.shell   :as shell   :refer [sh]]
             [clojure.data.json    :as json]
             [clojure.spec.alpha   :as s]
@@ -7,71 +11,37 @@
             [cognitect.anomalies  :as anom]
             [expound.alpha        :as expound :refer [expound-str]]
             [fc4.integrations.structurizr.express.spec :as ss]
-            [fc4.util             :as fu    :refer [namespaces]]))
+            [fc4.util             :as fu    :refer [namespaces]]
+            [fc4.yaml             :as yaml]))
 
 (namespaces '[structurizr :as st])
 
-(defn jar-dir
-  "Utility function to get the path to the dir in which the jar in which this
-  function is invoked is located.
-  Adapted from https://stackoverflow.com/a/13276993/7012"
+(def structurizr-express-url "https://structurizr.com/express")
+
+(defn start-renderer
   []
-  ;; The .toURI step is vital to avoid problems with special characters,
-  ;; including spaces and pluses.
-  ;; Source: https://stackoverflow.com/q/320542/7012#comment18478290_320595
-  (-> (class *ns*)
-      .getProtectionDomain .getCodeSource .getLocation .toURI .getPath
-      file .toPath .getParent .toFile))
+  "TODO"
+  (chrome/connect "localhost" 9222))
 
-(defn renderer-command
-  []
-  (let [possible-paths [; This first one must be first so it’s preferred to an
-                        ; “installed” executable when running tests from source
-                        "renderer/render.js"
-                        ; Used when the tool is packaged in a jar
-                        (str (file (jar-dir) "fc4-render"))
-                        "render"
-                        "target/pkg/renderer/render-mac"
-                        "target/pkg/renderer/render-macos"
-                        "target/pkg/renderer/render-linux"]
-        hopefully-on-path "fc4-render"]
-    (or (some #(if (.canExecute (file %)) %)
-              possible-paths)
-        hopefully-on-path)))
+(defn prep-yaml
+  "Structurizr Express will only recognize the YAML as YAML and parse it if
+  it begins with the YAML document separator. If this isn’t present, it will
+  assume that the diagram definition string is JSON and will fail."
+  [the-yaml]
+  (str "---\n" (::yaml/main (yaml/split-file the-yaml))))
 
-(defn get-fenced
-  "If fence is found, returns the fenced string; if not, throws."
-  [s sep]
-  (or (some-> (split s (re-pattern sep) 3)
-              (second)
-              (trim))
-      (throw (Exception. (str "Error finding fenced segments in error output: "
-                              s)))))
+(defn load-structurizr-express
+  [renderer]
+  (page/navigate renderer {:url structurizr-express-url})
+  ; (visible ) TODO
+  nil)
 
-; We have to capture this at compile time in order for it to have the value we
-; want it to; if we referred to *ns* in the body of a function then, because it
-; is dynamically bound, it would return the namespace at the top of the stack,
-; the “currently active namespace” rather than what we want, which is the
-; namespace of this file, because that’s the namespace all our keywords are
-; qualified with.
-(def ^:private this-ns-name (str *ns*))
-
-(defn parse-json-err
-  [js]
-  (try
-    (json/read-str js :key-fn (partial keyword this-ns-name))
-    (catch Exception e
-      (throw (if (includes? (.getMessage e) "JSON error")
-               (Exception. (str "Error while parsing JSON fenced by 🤖🤖🤖: " js)
-                           e)
-               e)))))
-
-(defn parse-stderr-err
-  "Parses the contents of stderr, presumably the output of a failed invocation
-  of the renderer, into a structured value."
-  [stderr]
-  {::human-output (get-fenced stderr "🚨🚨🚨")
-   ::error        (parse-json-err (get-fenced stderr "🤖🤖🤖"))})
+(defn set-yaml-and-update-diagram
+  [renderer yaml]
+  (js/evaluate renderer
+               {:expression
+                (str "const diagramYaml = `" yaml "`;\n"
+                     "structurizr.scripting.renderExpressDefinition(diagramYaml);")}))
 
 (s/def ::stderr string?)
 (s/def ::human-output string?)
@@ -80,35 +50,17 @@
 (s/def ::error (s/keys :req [::message]
                        :opt [::errors]))
 
-(s/fdef parse-stderr-err
-  :args ::stderr
-  :ret  (s/keys :req [::error ::human-output]))
-
 (defn render
   "Renders a Structurizr Express diagram as a PNG file, returning a PNG
-  bytearray on success. Not entirely pure; spawns a child process to perform the rendering.
-  FWIW, that process is stateless and ephemeral."
-  [diagram-yaml]
+  bytearray on success. Not entirely pure; communicates with a child process to perform the
+  rendering."
+  [renderer diagram-yaml]
   ;; Protect developers from themselves
   {:pre [(not (ends-with? diagram-yaml ".yaml"))
          (not (ends-with? diagram-yaml ".yml"))]}
-  ;; TODO: some way to pass options to the renderer (--debug, --quiet, --verbose)
-  ;; TODO: use ProcessBuilder (or some Clojure wrapper for such) rather than sh
-  ;; so we can stream output from stderr to stderr so we can display progress as
-  ;; it happens, so the user knows that something is actually happening!
-  (let [command (renderer-command)
-        result (sh command
-                   :in diagram-yaml
-                   :out-enc :bytes)
-        {:keys [exit out err]} result]
-    (if (zero? exit)
-      {::png-bytes out
-       ::stderr    err}
-      (let [{:keys [::human-output ::error]} (parse-stderr-err err)]
-        {::anom/category ::anom/fault
-         ::anom/message  human-output
-         ::stderr        err
-         ::error         error}))))
+  (let [prepped-yaml (prep-yaml diagram-yaml)]
+    (load-structurizr-express renderer)
+    (set-yaml-and-update-diagram renderer prepped-yaml)))
 
 (s/def ::png-bytes (s/and bytes? #(> (count %) 0)))
 (s/def ::result (s/keys :req [::png-bytes ::stderr]))
@@ -125,7 +77,7 @@
               :failure ::failure))
 
 (comment
-  (use 'clojure.java.io 'clojure.java.shell 'fc4.io.util)
+  (use 'clojure.java.io 'fc4.io.util)
   (require :reload '[fc4.integrations.structurizr.express.render :as r])
   (in-ns 'fc4.integrations.structurizr.express.render)
 
