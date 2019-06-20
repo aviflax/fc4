f11439d14e085abe67f2d4af15e63fac3522d2db
Avi Flax <avi.flax@fundingcircle.com>
2019-06-19 23:20:50 -0400

    [tool] Decouple two more features: edit + watch
    This is preparatory refactoring for #168.
    
    As part of #168, we’ll be making the watch a mode that’s composable with
    the main features, and we’ll be enabling users to toggle each of the
    main features individually. In other words, we have three major
    features: -f, -s, and -r. A user can invoke the tool with just one of
    those — any one, or with any two of them, or with all three. They can
    also optionally add -w to have the feature(s) applied in “watch mode”.
    
    This is substantially different than the current paradigm, wherein a
    specific and limited set of subcommands trigger a hard-coded “workflow”
    or mode of operation.
    
    In order to support the new approach, it is necessary to decouple the
    watch feature from edit workflow — essentially, to extract the watch
    feature from the edit workflow, and make it a higher-order function so
    that it can be composed with any workflow (or featureset, mode of
    operation, etc.) That’s what this change does.
    
    Most of the code related to watching is moved from edit.clj to watch.clj
    with minimal modifications. The biggest change is that the watch
    workflow can no longer assume that if the function it invokes changes
    the file that’d just been changed, that that change would be followed by
    a rendering step that’d block for a substantial amount of time — at
    least 300ms and often 1000–2500. And therefore we can no longer assume
    that when processing subsequent events, that the file will still be in
    the active set because it’s being rendered. Therefore I restored the map
    in the Hawk “context” value that keeps track of when a file change event
    was most recently triggered, for each given file. This had been removed
    in a5274963eb because at the time it proved sufficient to simply keep
    track of a current-active set and filter by membership in that set.
    
    For more background on how the watch has worked to date, and avoided
    duplicative work and infinite loops, see a5274963eb.
    
    Anyway, since the tests now supply processing functions that are
    extremely fast, and some of them change the subject file, I needed to
    restore the time-based successive-event filtering. I kept the active-set
    filtering for now, but I’m not sure it’s still needed. This is pretty
    hard to reason about, honestly.

--- tool/test/fc4/io/edit_test.clj
+++ tool/test/fc4/io/edit_test.clj
@@ -43,108 +43,104 @@
 
 (deftest format-and-snap
   (let [f #'e/format-and-snap
-        valid-before   "test/data/structurizr/express/diagram_valid_messy.yaml"
-        valid-expected "test/data/structurizr/express/diagram_valid_formatted_snapped.yaml"
-        invalid-a      "test/data/structurizr/express/se_diagram_invalid_a.yaml"
-        invalid-b      "test/data/structurizr/express/se_diagram_invalid_b.yaml"]
+        valid-before       "test/data/structurizr/express/diagram_valid_messy.yaml"
+        valid-expected     "test/data/structurizr/express/diagram_valid_formatted_snapped.yaml"
+        invalid-a          "test/data/structurizr/express/se_diagram_invalid_a.yaml"
+        invalid-b          "test/data/structurizr/express/se_diagram_invalid_b.yaml"
+        invalid-b-expected "test/data/structurizr/express/se_diagram_invalid_b_formatted_snapped.yaml"]
     (testing "a YAML string containing a valid SE diagram"
       (is (= (slurp valid-expected)
              (f (slurp valid-before)))))
     (testing "a YAML string containing a blatantly invalid SE diagram"
       (let [yaml (slurp invalid-a)]
+        ; We compare the main documents only so we can ignore the comments at the top of the files.
         (is (= (-> yaml split-file ::fy/main parse-string)
                (-> yaml f split-file ::fy/main parse-string)))))
     (testing "a YAML string containing a subtly invalid SE diagram"
-      (let [yaml (slurp invalid-b)]
-        (is (= (-> yaml split-file ::fy/main parse-string)
-               (-> yaml f split-file ::fy/main parse-string)))))
+      ; ...invalid to Structurizr Express but not to this tool.
+      ; We compare the main documents only so we can ignore the comments at the top of the files.
+      (is (= (-> invalid-b-expected slurp   split-file ::fy/main)
+             (-> invalid-b          slurp f split-file ::fy/main))))
     (testing "an empty string"
       (is (nil? (f ""))))))
 
-;; The tests below don’t thoroughly check the specific features that are called by the edit workflow
-;; (formatting, snapping, and rendering) because those have their own focused tests already.
-;; Therefore it’s sufficient for these tests to simply confirm that those features have been
-;; invoked.
-
-;; It’s common for tests to be broader than these, and to use `testing` to test
-;; various scenarios. These tests are smaller and more specific, using basically
-;; one deftest per scenario, because they are very slow, and these test
-;; functions are our units of concurrency. By breaking them into multiple
-;; top-level deftests, they can run in parallel.
-
-(deftest edit-workflow-single-file-single-change
-  (testing "changing a single file once"
-    (let [yaml-expected "test/data/structurizr/express/diagram_valid_formatted_snapped.yaml"
-          yaml-input "test/data/structurizr/express/diagram_valid_messy.yaml"
-          yaml-file (tmp-copy yaml-input)
+(deftest process-file
+  ;; These tests don’t thoroughly check the specific features that are called by the edit workflow
+  ;; (formatting, snapping, and rendering) because those have their own focused tests already.
+  ;; Therefore it’s sufficient for these tests to simply confirm that those features have been
+  ;; invoked.
+  (testing "simple happy path"
+    (let [yaml-file (tmp-copy "test/data/structurizr/express/diagram_valid_messy.yaml")
+          yaml-expected "test/data/structurizr/express/diagram_valid_formatted_snapped.yaml"
           png-file (file (set-extension yaml-file "png"))
           yaml-file-size-before (.length yaml-file)
           _ (is (or (not (.exists png-file))
                     (.delete png-file)))
-          watch (atom nil)
           output (with-out-str
-                   (reset! watch (e/start (str yaml-file)))
-                   (Thread/sleep 100)
-                   (append yaml-file "\n")
-                   (Thread/sleep 12000))]
-      (e/stop @watch)
+                   (e/process-file yaml-file))]
       (is (.exists png-file))
       (is (not= yaml-file-size-before (.length yaml-file)))
       (is (= (slurp yaml-expected) (slurp yaml-file)))
       (is (<= 50000 (.length png-file)))
-      (is (= 3 (count-substring output "✅")))
-      (is (= 2 (count (split-lines output))))
+      (is (= 2 (count-substring output "✅")))
+      (is (= 0 (count-substring output "🚨")))
+      (is (= 1 (count (split-lines output))))
       (delete-file yaml-file)
-      (delete-file png-file))))
-
-(deftest edit-workflow-error-should-not-break-watch
-  ;; We had a bug wherein if an exception was thrown while rendering — and the
-  ;; current workflow does use exceptions, to my regret — further changes to
-  ;; that file would not trigger processing.
-  (testing "a rendering error should not break the watch for that file"
-    (let [yaml-file (tmp-copy "test/data/structurizr/express/se_diagram_invalid_c.yaml")
-          watch (atom nil)
+      (delete-file png-file)))
+  (testing "sad path: formatting+snapping succeeds, but rendering fails"
+    (let [yaml-file (tmp-copy "test/data/structurizr/express/se_diagram_invalid_b.yaml")
+          yaml-expected "test/data/structurizr/express/se_diagram_invalid_b_formatted_snapped.yaml"
+          png-file (file (set-extension yaml-file "png"))
+          yaml-file-size-before (.length yaml-file)
+          _ (is (or (not (.exists png-file))
+                    (.delete png-file)))
           output (with-out-str
-                   (reset! watch (e/start (str yaml-file)))
-                   (Thread/sleep 100)
-                   (append yaml-file "\n")
-                   (Thread/sleep 5000)
-                   (append yaml-file "\n")
-                   (Thread/sleep 5000))]
-      (e/stop @watch)
-      (is (= 4 (count-substring output "✅")))
-      (is (= 2 (count-substring output "🚨")))
-      (is (= 2 (count-substring output "💀")))
-      (is (= 5 (count (split-lines output))))
+                   (is (thrown-with-msg?
+                        Exception
+                        #"(?s)Error processing.+RENDERING FAILED.+Errors were found in the diagram definition.+diagram type must be"
+                        (e/process-file yaml-file))))]
+      (is (not (.exists png-file)))
+      (is (not= yaml-file-size-before (.length yaml-file)))
+      ; We compare the main documents only so we can ignore the comments at the top of the files.
+      (is (= (-> yaml-expected slurp split-file ::fy/main)
+             (-> yaml-file     slurp split-file ::fy/main)))
+      (is (= 2 (count-substring output "✅")))
+      (is (= 0 (count-substring output "🚨")))
+      (is (= 1 (count (split-lines output))))
+      (delete-file yaml-file)))
+  (testing "sad path: file contents are subtly invalid, so validation passes, but
+           formatting/snapping fails"
+    (let [original-file "test/data/structurizr/express/se_diagram_invalid_d.yaml"
+          yaml-file (tmp-copy original-file)
+          png-file (file (set-extension yaml-file "png"))
+          yaml-file-size-before (.length yaml-file)
+          _ (is (or (not (.exists png-file))
+                    (.delete png-file)))
+          output (with-out-str
+                   (is (thrown? Exception
+                                (e/process-file yaml-file))))]
+      (is (not (.exists png-file)))
+      (is (= yaml-file-size-before (.length yaml-file)))
+      (is (= (slurp original-file) (slurp yaml-file)))
+      (is (= 1 (count-substring output "✅")))
+      (is (= 0 (count-substring output "🚨")))
+      (is (= 1 (count (split-lines output))))
+      (delete-file yaml-file)))
+  (testing "sad path: file contents are blatantly invalid, so validation should throw, and no
+           formatting, snapping or rendering should occur"
+    (let [yaml-file (tmp-copy "test/data/structurizr/express/se_diagram_invalid_a.yaml")
+          png-file (file (set-extension yaml-file "png"))
+          yaml-file-size-before (.length yaml-file)
+          _ (is (or (not (.exists png-file))
+                    (.delete png-file)))
+          output (with-out-str
+                   (is (thrown-with-msg?
+                        Exception
+                        #"(?s)Error processing.+Spec failed.+invalid because it is missing the root property.+scope"
+                        (e/process-file yaml-file))))]
+      (is (not (.exists png-file)))
+      (is (= yaml-file-size-before (.length yaml-file)))
+      (is (= 0 (count-substring output "✅")))
+      (is (= 0 (count-substring output "🚨")))
+      (is (= 1 (count (split-lines output))))
       (delete-file yaml-file))))
-
-(deftest edit-workflow-two-files-changed-simultaneously
-  (testing "changing two files simultaneously"
-    (let [yaml-expected "test/data/structurizr/express/diagram_valid_formatted_snapped.yaml"
-          yaml-input "test/data/structurizr/express/diagram_valid_messy.yaml"
-          yaml-files (repeatedly 2 #(tmp-copy yaml-input))
-          png-files (map #(file (set-extension % "png")) yaml-files)
-          yaml-file-size-before (.length (first yaml-files)) ; they’re identical
-          _ (doseq [png-file png-files]
-              (is (or (not (.exists png-file))
-                      (.delete png-file))))
-          watch (atom nil)
-          output (with-out-str
-                   (reset! watch (apply e/start yaml-files))
-                   (Thread/sleep 100)
-                   (run! #(append % "\n") yaml-files)
-                   (Thread/sleep 18000))]
-      (e/stop @watch)
-      (is (= 6 (count-substring output "✅"))
-          (str "Output should have had 6 ✅:\n"
-               output
-               "\n»» Maybe rendering timed out or just took longer than the sleep above?"))
-      (is (= 3 (count (split-lines output))))
-      (doseq [png-file png-files]
-        (is (.exists png-file))
-        (is (<= 50000 (.length png-file)))
-        (delete-file png-file))
-      (doseq [yaml-file yaml-files]
-        (is (not= (.length yaml-file) yaml-file-size-before))
-        (is (= (slurp yaml-expected) (slurp yaml-file)))
-        (delete-file yaml-file)))))
