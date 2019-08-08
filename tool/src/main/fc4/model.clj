e729ac9113fce1903f28fb6433f7893b7750edc7
Avi Flax <avi.flax@fundingcircle.com>
2019-08-08 15:22:57 +0100

    POSSIBLY CORRECT: CLEAN UP after rebase
    There were a TON of conflicts AND <dog-at-keyboard-i-dont-know-what-im-doing.jpg>

--- tool/src/main/fc4/model.clj
+++ tool/src/main/fc4/model.clj
@@ -1,13 +1,4 @@
-(ns fc4.model
-  (:require [clj-yaml.core :as yaml]
-            [clojure.set :refer [union]]
-            [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as gen]
-            [clojure.string :refer [includes? split]]
-            [fc4.dsl :as dsl]
-            [fc4.files :refer [relativize]]
-            [fc4.spec :as fs]
-            [fc4.util :as fu :refer [qualify-keys]]))
+(ns fc4.model)
 
 ;; TODO: now that _this_ file, model.clj, is basically empty, let’s just move
 ; the contents of model_specs.clj over into this file.
