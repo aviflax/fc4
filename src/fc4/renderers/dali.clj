a3a41b691923731fcd6081805c5386f729add263
Avi Flax <avi.flax@fundingcircle.com>
2020-01-26 16:30:27 -0500

    WIP++

--- src/fc4/renderers/dali.clj
+++ src/fc4/renderers/dali.clj
@@ -7,10 +7,12 @@
             [dali.layout.stack] ; for side-fx
             [dali.prefab :as dp]
             [dali.syntax :as ds]
+            [fc4.io.dsl :as fid]
             [fc4.rendering :as r]
             [fc4.util :as u]))
 
-(u/namespaces '[fc4 :as f])
+(u/namespaces '[fc4 :as f]
+              '[fc4.view :as v])
 
 (def canvas-sizes
   "Map of canvas sizes to dimensions."
@@ -30,24 +32,24 @@
             10]
      (text-stack (str/split-lines text))]])
 
-
-; (defn- elements
-;   [view model]
-;   (cons
-;      )
-;   )
+(defn- elements
+  [view _model]
+  ; [(element "A thing" "Marketplace" :system [10 10])]
+  ; (clojure.pprint/pprint (get-in view [::v/positions ::v/containers]))
+  (mapv (fn [[elem-name pos]] (element elem-name elem-name :system (vec pos)))
+        (get-in view [::v/positions ::v/containers])))
 
 (defn render
   "Renders an FC4 view on an FC4 model. Returns either an fc4.rendering/failure-result (which is a
   cognitect.anomalies/anomaly) or an fc4.rendering/success-result."
-  [view model opts]
-  [:dali/page
-    [:circle [50 50] 40]]
-  )
+  [view model _opts]
+  (into [:dali/page
+          [:defs (dp/drop-shadow-effect :ds {:opacity 0.3 :offset [5 5] :radius 6})]]
+    (elements view model)))
 
 (s/def ::options map?)
 
-(s/defn render
+(s/fdef render
   :args (s/cat :view  ::f/view
                :model ::f/model
                :opts  ::options)
@@ -57,26 +59,26 @@
 
 (comment
 
-(require '[dali.io :as dio])
+  ; (require '[dali.io :as dio])
 
-(defn tr [doc]
-  (dio/render-svg doc "test.svg")
-  (dio/render-png doc "test.png"))
+  (defn tr [doc]
+    (dio/render-svg doc "test.svg")
+    (dio/render-png doc "test.png"))
 
-(tr
-  (let [faint-grey "#777777"
-        defs [:defs
-               (dp/drop-shadow-effect :ds {:opacity 0.3 :offset [5 5] :radius 6})
-               ; (ds/css [[:rect {:fill :none :stroke faint-grey}]
-               ;          [:.box-text {:stroke :black}]])
-                        ]]
-     [:dali/page
-       defs
-       (element "A thing" "Marketplace" :system [10 10])
-       (element "B" "BILCAS" :service [200 10])
-     ]))
+  (let [v (fid/read-view "test/data/views/middle (valid).yaml")
+        m (fid/read-model "test/data/models/valid/a/flat")
+        d (render v m nil)]
+    (tr d))
 
-
-
-
-  )
+  (tr
+    (let [faint-grey "#777777"
+          defs [:defs
+                 (dp/drop-shadow-effect :ds {:opacity 0.3 :offset [5 5] :radius 6})
+                 ; (ds/css [[:rect {:fill :none :stroke faint-grey}]
+                 ;          [:.box-text {:stroke :black}]])
+                          ]]
+       [:dali/page
+         defs
+         (element "A thing" "Marketplace" :system [10 10])
+         (element "B" "BILCAS" :service [200 10])
+       ])))
