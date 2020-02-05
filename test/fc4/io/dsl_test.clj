eb971f8eb8ff39f5bc0a3bbc2ac206daadf9ac94
Avi Flax <avi.flax@fundingcircle.com>
2020-02-05 16:25:16 -0500

    Fix some failing tests related to model loading
    In the prior commit I enabled clojure.spec instrumentation within the
    test ns `fc4.io.dsl-test` to make the testing more rigorous. This
    immediately caused the previously-existing test to start failing,
    because the paths that were being supplied to the function under test as
    args didn’t conform to the spec for the arg, as specified by the fdef
    for the function. This fixes those tests by fixing those args such that
    they now conform to the specs.

--- test/fc4/io/dsl_test.clj
+++ test/fc4/io/dsl_test.clj
@@ -34,10 +34,10 @@
 (deftest read-model
   (testing "happy paths:"
     (testing "valid models"
-      (doseq [dir ["test/data/models/valid/a/flat"
-                   "test/data/models/valid/a/nested"
-                   "test/data/models/valid/b/single-file"
-                   "test/data/models/valid/b/split"]]
+      (doseq [dir ["test/data/models/valid/a/flat/"
+                   "test/data/models/valid/a/nested/"
+                   "test/data/models/valid/b/single-file/"
+                   "test/data/models/valid/b/split/"]]
         (let [result (dsl/read-model dir)]
           (is (s/valid? ::f/model result)
               (if-let [msg (::anom/message result)]
@@ -45,46 +45,46 @@
                 (expound-str ::f/model result))))))
 
     (testing "flat and nested models should be equal once read"
-      (is (= (dsl/read-model "test/data/models/valid/a/flat")
-             (dsl/read-model "test/data/models/valid/a/nested"))))
+      (is (= (dsl/read-model "test/data/models/valid/a/flat/")
+             (dsl/read-model "test/data/models/valid/a/nested/"))))
 
     (testing "a system definition should be identical whether defined in a single file or split
               across multiple files"
-      (is (= (dsl/read-model "test/data/models/valid/b/single-file")
-             (dsl/read-model "test/data/models/valid/b/split"))))
+      (is (= (dsl/read-model "test/data/models/valid/b/single-file/")
+             (dsl/read-model "test/data/models/valid/b/split/"))))
 
     (testing "some spot checks of models read from disk"
       (is (= "A bank account of some kind."
-             (get-in (dsl/read-model "test/data/models/valid/b/single-file")
+             (get-in (dsl/read-model "test/data/models/valid/b/single-file/")
                      [::m/systems "Marketplace" ::m/systems "Exchequer" ::m/description])))
 
       (is (= {"foo" "bar", "baz" "quux"}
-             (get-in (dsl/read-model "test/data/models/valid/b/split")
+             (get-in (dsl/read-model "test/data/models/valid/b/split/")
                      [::m/systems "Marketplace" ::m/tags])))
 
       (is (= "qBne47S"
-             (get-in (dsl/read-model "test/data/models/valid/a/nested")
+             (get-in (dsl/read-model "test/data/models/valid/a/nested/")
                      [::m/systems "Internal" ::m/containers "External" ::m/description])))
 
       (is (= "Websockets"
-             (get-in (dsl/read-model "test/data/models/valid/a/nested")
+             (get-in (dsl/read-model "test/data/models/valid/a/nested/")
                      [::m/systems "Middle" ::m/uses "Front" ::m/protocol])))))
 
   (testing "sad path:"
     (testing "files on disk contain invalid data as per the specs"
-      (let [result (dsl/read-model "test/data/models/invalid/a")]
+      (let [result (dsl/read-model "test/data/models/invalid/a/")]
         (is (not (s/valid? ::f/model result)))
         (is (s/valid? ::dsl/anomaly result))))
 
     (testing "a file is malformed (it is not valid YAML)"
-      (let [result (dsl/read-model "test/data/models/invalid/malformed")]
+      (let [result (dsl/read-model "test/data/models/invalid/malformed/")]
         (is (not (s/valid? ::f/model result)))
         (is (s/valid? ::dsl/anomaly result))))
 
     (testing "dir does not exist:"
       (is (thrown-with-msg? FileNotFoundException
                             #"root"
-                            (dsl/read-model "foo/bar/root"))))
+                            (dsl/read-model "foo/bar/root/"))))
 
     (testing "supplied root path is to a file"
       (let [to-unstrument ['fc4.io.dsl/read-model
