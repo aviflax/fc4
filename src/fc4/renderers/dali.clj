3ab796845d08b4b388a888cff29d7f3cea2d177b
Avi Flax <avi.flax@fundingcircle.com>
2020-02-19 20:27:14 -0500

    WIP++

--- src/fc4/renderers/dali.clj
+++ src/fc4/renderers/dali.clj
@@ -18,12 +18,11 @@
 (def elem-height 100)
 (def font-family "Fira Sans")
 (def font-size 20)
-
 (def canvas-sizes
   "Map of canvas sizes to dimensions."
-  {"A4_Landscape" [600 400]})
-
+  {"2000" [2000 2000]})
 (def center-pos [900 900])
+(def elem-margin 50)
 
 (defn- text-stack [texts]
   (into [:dali/stack {:direction :down :gap 6}]
@@ -31,21 +30,23 @@
 
 (defn- system-boundary
   "Given a view, compute the boundaries needed for the system by examining the containers included
-  in the diagram. Returns a map with :x :y :w :h"
+  in the diagram. Returns a dali rect vector."
   [view]
   (let [container-positions (vals (get-in view [::v/positions ::v/containers]))
         container-xs (map first container-positions)
-        container-ys (map second container-positions)]
-    ; each container position represents the x,y of the upper-left hand corner of the container.
-    ; so first we just need to find the leftmost container pos and the topmost pos to find the x and
-    ; y we will return.
-    {:x (apply min container-xs)
-     :y (apply min container-ys)
-     ; now we need the rightmost and the bottom-most.
-     :w (+ (apply max container-xs) elem-width)
-     :h (+ (apply max container-ys) elem-height)}))
+        container-ys (map second container-positions)
+        ; each container position represents the x,y of the upper-left hand corner of the container.
+        ; so first we just need to find the leftmost container pos and the topmost pos to find the x and
+        ; y we will return.
+        lx (- (apply min container-xs) elem-margin)
+        ly (- (apply min container-ys) elem-margin)
 
-
+        ; now we need the rightmost and the bottom-most.
+        rx (+ (apply max container-xs) elem-width elem-margin)
+        w (- rx lx)
+        ry (+ (apply max container-ys) elem-height elem-margin)
+        h (- ry ly)]
+     [:rect {:fill :none :stroke "#777777"} [lx ly] [w h]]))
 
 (defn- element
   [id text cl pos]
@@ -63,7 +64,7 @@
   [view _model]
   (map (fn [[elem-name pos]] (element elem-name elem-name :system (vec pos)))
        (merge
-         {(get view ::v/system) center-pos}
+         ; {(get view ::v/system) center-pos} ; only for context diagrams
          (get-in view [::v/positions ::v/containers])
          (get-in view [::v/positions ::v/systems]))))
 
@@ -73,7 +74,8 @@
   [view model _opts]
   (into [:dali/page
           [:defs (dp/drop-shadow-effect :ds {:opacity 0.3 :offset [5 5] :radius 6})]]
-    (elements view model)))
+    (cons (system-boundary view)
+      (elements view model))))
 
 (s/def ::options map?)
 
@@ -95,18 +97,4 @@
     (let [doc (render view model nil)]
       ; (clojure.pprint/pprint doc)
       ; (dio/render-svg doc "test.svg")
-      (dio/render-png doc "test.png")))
-
-  ; (tr
-  ;   (let [faint-grey "#777777"
-  ;         defs [:defs
-  ;                (dp/drop-shadow-effect :ds {:opacity 0.3 :offset [5 5] :radius 6})
-  ;                ; (ds/css [[:rect {:fill :none :stroke faint-grey}]
-  ;                ;          [:.box-text {:stroke :black}]])
-  ;                         ]]
-  ;      [:dali/page
-  ;        defs
-  ;        (element "A thing" "Marketplace" :system [10 10])
-  ;        (element "B" "BILCAS" :service [200 10])
-  ;      ]))
-)
+      (dio/render-png doc "test.png"))))
